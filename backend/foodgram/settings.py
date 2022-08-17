21c834ef1b8bb75b8430116a3d6ea64ca419fdd7
shakdv <shakdv@yandex.ru>
2022-08-17 09:56:43 +0500

    Update settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'super_secret_key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
